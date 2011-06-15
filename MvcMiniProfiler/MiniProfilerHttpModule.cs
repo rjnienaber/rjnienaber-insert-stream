ced0a6e1995cfd6655349eb6c58f424450287a7b
Richard Nienaber <rjnienaber@gmail.com>
2011-06-15 11:52:30 +0100

    Added InsertMarkupFilter so that you don't need to explicitly include markup in master pages Added unit test project to test InsertMarkupFilter

---
+++ MvcMiniProfiler/MiniProfilerHttpModule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using MvcMiniProfiler.Filter;
+using System.IO;
+
+namespace MvcMiniProfiler
+{
+    public class MiniProfilerHttpModule : IHttpModule
+    {
+        public void Dispose()
+        {
+            
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.ReleaseRequestState += ReleaseRequestState;
+        }
+
+        public void ReleaseRequestState(object sender, EventArgs e)
+        {
+            var context = HttpContext.Current;
+            var response = context.Response;
+            if (response.ContentType != "text/html" || IsARedirect(response))
+                return;
+
+            var filter = InsertMarkupFilter.InterceptResponse(response);
+            filter.EndOfBodyDetected += writer => writer.Write(MiniProfiler.RenderIncludes().ToHtmlString());
+        }
+
+        bool IsARedirect(HttpResponse response)
+        {
+            return response.StatusCode == 301 || response.StatusCode == 302;
+        }
+    }
+}
